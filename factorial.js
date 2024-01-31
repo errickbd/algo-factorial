ba62a20a8356f7ea5e8d240ae028e939cff0fe33
Errick Basa-Denis <errickbd@gmail.com>
2024-01-31 12:18:21 -0600

    added json files

--- factorial.js
+++ factorial.js
@@ -1,5 +1,9 @@
 function factorial(num) {
-  return 0;
+
+  return ([...Array(num + 1).keys()].slice(1)).reduce((accumulator, currentValue) => accumulator * currentValue, );
 }
 
 module.exports = factorial;
+
+console.log(factorial(4))
+// .reduce((accumulator, currentValue) => accumulator * currentValue);
